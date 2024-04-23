0d3fae06f706ed90082191b679f236c3f61addbe
Didar5501 <didar5501@gmail.com>
2024-04-23 20:12:33 +0500

    23.04.2024v4

--- src/avto_cc/views.py
+++ src/avto_cc/views.py
@@ -1,8 +1,70 @@
+# from django.shortcuts import render
+# from django.views.generic import ListView, DetailView
+# from django.core.paginator import Paginator
+# from .models import mcfcarbrand, mcfcarmodel, mcfcarcolor
+
+
+# class McfCarBrandListView(ListView):
+#     model = mcfcarbrand
+#     template_name = 'avto_cc/mcfcarbrand_list.html'
+#     context_object_name = 'ccbrands'
+#     paginate_by = 10  
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         name_filter = self.request.GET.get('name_filter') 
+#         if name_filter:
+#             queryset = queryset.filter(Name__icontains=name_filter)
+#         return queryset
+
+# from django.views.generic import ListView
+# from .models import mcfcarmodel, mcfcarbrand
+
+# class McfCarModelListView(ListView):
+#     model = mcfcarmodel
+#     template_name = 'avto_cc/mcfcarmodel_list.html'
+#     context_object_name = 'ccmodels'
+#     paginate_by = 10  
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         name_filter = self.request.GET.get('name_filter') 
+#         brand_id = self.request.GET.get('brand_id') 
+#         if brand_id:
+#             queryset = queryset.filter(carbrand=brand_id)
+#         if name_filter:
+#             queryset = queryset.filter(Name__icontains=name_filter)
+#         return queryset
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['ccbrands'] = mcfcarbrand.objects.all()  
+#         return context
+
+
+
+# class McfCarColorListView(ListView):
+#     model = mcfcarcolor
+#     queryset = mcfcarcolor.objects.all()
+#     template_name = 'avto_cc/mcfcarcolor_list.html'
+#     context_object_name = 'cccolors'
+
+
+# class CarBrandDetailView(DetailView):
+#     model = mcfcarbrand
+#     template_name = 'avto_cc/car_brand_detail.html'
+#     context_object_name = 'brand'
+
+
+
+# class CarModelDetailView(DetailView):
+#     model = mcfcarmodel
+#     template_name = 'avto_cc/car_model_detail.html'
+#     context_object_name = 'model'
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator
 from .models import mcfcarbrand, mcfcarmodel, mcfcarcolor
-
 
 class McfCarBrandListView(ListView):
     model = mcfcarbrand
@@ -15,10 +77,7 @@ class McfCarBrandListView(ListView):
         name_filter = self.request.GET.get('name_filter') 
         if name_filter:
             queryset = queryset.filter(Name__icontains=name_filter)
-        return queryset
-
-from django.views.generic import ListView
-from .models import mcfcarmodel, mcfcarbrand
+        return queryset.select_related('country', 'creationauthor', 'changeauthor')
 
 class McfCarModelListView(ListView):
     model = mcfcarmodel
@@ -34,14 +93,12 @@ class McfCarModelListView(ListView):
             queryset = queryset.filter(carbrand=brand_id)
         if name_filter:
             queryset = queryset.filter(Name__icontains=name_filter)
-        return queryset
+        return queryset.select_related('carbrand', 'creationauthor', 'changeauthor')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['ccbrands'] = mcfcarbrand.objects.all()  # Получаем список всех брендов
+        context['ccbrands'] = mcfcarbrand.objects.all().select_related('country', 'creationauthor', 'changeauthor')
         return context
-
-
 
 class McfCarColorListView(ListView):
     model = mcfcarcolor
@@ -49,15 +106,21 @@ class McfCarColorListView(ListView):
     template_name = 'avto_cc/mcfcarcolor_list.html'
     context_object_name = 'cccolors'
 
+    def get_queryset(self):
+        return super().get_queryset().select_related('creationauthor', 'changeauthor')
 
 class CarBrandDetailView(DetailView):
     model = mcfcarbrand
     template_name = 'avto_cc/car_brand_detail.html'
     context_object_name = 'brand'
 
-
+    def get_queryset(self):
+        return super().get_queryset().select_related('country', 'creationauthor', 'changeauthor')
 
 class CarModelDetailView(DetailView):
     model = mcfcarmodel
     template_name = 'avto_cc/car_model_detail.html'
     context_object_name = 'model'
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('carbrand', 'creationauthor', 'changeauthor')
