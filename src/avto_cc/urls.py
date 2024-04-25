fe8d41c28ad53519425966e98d753c261438d6d1
Didar5501 <didar5501@gmail.com>
2024-04-26 03:17:18 +0500

    ok

--- src/avto_cc/urls.py
+++ src/avto_cc/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import McfCarBrandListView, McfCarModelListView, McfCarColorListView, CarBrandDetailView, CarModelDetailView
 from avto_bs.views import DeleteCarBrandView
+from .views import weather_view
+
 
 urlpatterns = [
     path('mcfcarbrands/', McfCarBrandListView.as_view(), name='mcfcarbrand_list'),
@@ -8,9 +10,7 @@ urlpatterns = [
     path('mcfcarcolors/', McfCarColorListView.as_view(), name='mcfcarcolor_list'),
     path('car_brand_detail/<int:pk>/', CarBrandDetailView.as_view(), name='car_brand_detail'),
     path('car_model_detail/<int:pk>/', CarModelDetailView.as_view(), name='car_model_detail'),
-
-
-    
+    path('weather/', weather_view, name='weather'),
 
 
 ]
